fc8ec03d5033939b2dc079560cf09f519f70cb80
e802 <82760885+e802@users.noreply.github.com>
2023-05-08 17:47:47 +0530

    Update BottomTabNavigator.js
    Explaining Stack Based navigation system

--- components/BottomTabNavigator.js
+++ components/BottomTabNavigator.js
@@ -5,6 +5,15 @@ import { NavigationContainer } from "@react-navigation/native";
 // In React Native, the NavigationContainer is a component provided by the React Navigation library
 // that helps manage navigation between screens in your application. 
 // It creates a navigation context and provides a way to navigate between screens using a stack-based navigation system.
+// A stack-based navigation system is a type of navigation system used in mobile apps that maintains a stack of screens or views. 
+// In this system, when a user navigates to a new screen, it is added to the top of the stack. 
+// When the user navigates back, the top screen is removed from the stack and the user is returned to the previous screen.
+
+Stack-based navigation systems are commonly used in mobile app development frameworks such as React Native and Flutter. They provide a simple and intuitive way for users to navigate through an app's content and allow for a clear and predictable back button behavior.
+
+In a stack-based navigation system, the stack is usually implemented as a Last-In-First-Out (LIFO) data structure. This means that the most recently added screen is the first one to be removed when the user navigates back.
+
+Stack-based navigation systems often provide additional features such as navigation animations and transitions, as well as support for nested navigation and modal screens. Overall, stack-based navigation systems are a powerful and versatile tool for building mobile apps with multiple screens and views.
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import TransactionScreen from "../screens/Transaction";
