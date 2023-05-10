7e79398abc648749fb5e5f9de4d3c8b706dc1e3c
e802 <82760885+e802@users.noreply.github.com>
2023-05-10 12:37:02 +0530

    Update Search.js
    flex:1 explanation added as a comment

--- screens/Search.js
+++ screens/Search.js
@@ -23,3 +23,9 @@ const styles = StyleSheet.create({
     fontSize: 30
   }
 });
+
+// In React Native, flex: 1 is a common style property used to control how a component expands or shrinks to fill the available space within 
+// its parent container.
+// When applied to a component, the flex: 1 style property tells the component to expand and fill as much available space as possible 
+// within its parent container. 
+// This means that the component will stretch to fill the remaining space that is not occupied by other components or content.
